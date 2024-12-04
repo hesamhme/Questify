e80f4a54eaa307db6efa2a13fdbd0c87b27583cf
Hesam Hashemi <109273285+hesamhme@users.noreply.github.com>
2024-12-05 00:56:24 +0330

    fix: add realations

--- pkg/adapters/storage/entities/answers.go
+++ pkg/adapters/storage/entities/answers.go
@@ -8,8 +8,8 @@ import (
 
 type Answer struct {
 	ID         uuid.UUID `gorm:"type:uuid;default:uuid_generate_v4();primaryKey"`
-	QuestionID uuid.UUID `gorm:"type:uuid;not null"`
-	UserID     uuid.UUID `gorm:"type:uuid;not null"`
+	QuestionID Question `gorm:"foreignKey:ID;references:ID"` 
+	UserID     User `gorm:"foreignKey:UserID;references:ID"`
 	Response   string    `gorm:"type:text;not null"`
 	CreatedAt  time.Time `gorm:"autoCreateTime"`
 }
