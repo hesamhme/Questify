6ae7cd4a7ce695979b3159c965e308881b4c0c7a
Aryagorjipour <arygorji.pour@gmail.com>
2024-12-07 23:56:12 +0330

    feat: add random system to show question to user per survey id

--- service/survey.go
+++ service/survey.go
@@ -6,26 +6,34 @@ import (
 	"context"
 	"fmt"
 	"github.com/google/uuid"
+	"math/rand"
 	"sync"
+	"time"
 )
 
-type SurveyService struct {
-	questionOps  *question.Ops
-	surveyOps    *survey.Ops
-	mu           sync.Mutex
-	userProgress map[string]uint
+func init() {
+	rand.Seed(time.Now().UnixNano())
 }
 
-func generateProgressKey(userID string, surveyID uuid.UUID) string {
-	return fmt.Sprintf("%s:%s", userID, surveyID.String())
+type SurveyService struct {
+	questionOps    *question.Ops
+	surveyOps      *survey.Ops
+	mu             sync.Mutex
+	userProgress   map[string]uint
+	shownQuestions map[string][]uuid.UUID
 }
 
 func NewSurveyService(questionOps *question.Ops, surveyOps *survey.Ops) *SurveyService {
 	return &SurveyService{
-		questionOps:  questionOps,
-		surveyOps:    surveyOps,
-		userProgress: make(map[string]uint),
+		questionOps:    questionOps,
+		surveyOps:      surveyOps,
+		userProgress:   make(map[string]uint),
+		shownQuestions: make(map[string][]uuid.UUID),
 	}
+}
+
+func generateProgressKey(userID string, surveyID uuid.UUID) string {
+	return fmt.Sprintf("%s:%s", userID, surveyID.String())
 }
 
 func (s *SurveyService) CreateQuestion(ctx context.Context, question *question.Question) error {
@@ -33,62 +41,117 @@ func (s *SurveyService) CreateQuestion(ctx context.Context, question *question.Q
 }
 
 func (s *SurveyService) GetNextQuestion(ctx context.Context, surveyID uuid.UUID, userID string) (*question.Question, error) {
-    s.mu.Lock()
-    progressKey := generateProgressKey(userID, surveyID)
-    currentIndex := s.userProgress[progressKey]
-    s.userProgress[progressKey]++
-    s.mu.Unlock()
-
-    questions, err := s.questionOps.GetQuestionsBySurveyID(ctx, surveyID)
-    if err != nil {
-        return nil, err
-    }
-
-    if int(currentIndex) >= len(questions) {
-        return nil, fmt.Errorf("no more questions")
-    }
-
-    return questions[currentIndex], nil
-}
-
-func (s *SurveyService) ResetUserProgress(userID string) {
 	s.mu.Lock()
-	delete(s.userProgress, userID)
-	s.mu.Unlock()
+	defer s.mu.Unlock()
+
+	survey, err := s.surveyOps.GetByID(ctx, surveyID)
+	if err != nil {
+		return nil, err
+	}
+
+	questions, err := s.questionOps.GetQuestionsBySurveyID(ctx, surveyID)
+	if err != nil {
+		return nil, err
+	}
+
+	progressKey := generateProgressKey(userID, surveyID)
+	shownQuestionsKey := progressKey
+
+	if len(questions) == 0 {
+		return nil, fmt.Errorf("no questions available for this survey")
+	}
+
+	var nextQuestion *question.Question
+
+	if survey.IsRandom {
+		notShownQuestions := getNotShownQuestions(questions, s.shownQuestions[shownQuestionsKey])
+
+		if len(notShownQuestions) == 0 {
+			return nil, fmt.Errorf("all questions have been shown")
+		}
+
+		nextQuestion = notShownQuestions[rand.Intn(len(notShownQuestions))]
+	} else {
+		currentIndex := s.userProgress[progressKey]
+		if int(currentIndex) >= len(questions) {
+			return nil, fmt.Errorf("no more questions")
+		}
+		nextQuestion = questions[currentIndex]
+		s.userProgress[progressKey]++
+	}
+
+	s.shownQuestions[shownQuestionsKey] = append(s.shownQuestions[shownQuestionsKey], nextQuestion.ID)
+
+	return nextQuestion, nil
 }
 
 func (s *SurveyService) GetPreviousQuestion(ctx context.Context, surveyID uuid.UUID, userID string) (*question.Question, error) {
-    resultSurvey, err := s.surveyOps.GetByID(ctx, surveyID)
-    if err != nil {
-        return nil, err
-    }
+	s.mu.Lock()
+	defer s.mu.Unlock()
 
-    if !resultSurvey.AllowBack {
-        return nil, fmt.Errorf("going back is not allowed for this survey")
-    }
+	survey, err := s.surveyOps.GetByID(ctx, surveyID)
+	if err != nil {
+		return nil, err
+	}
 
-    s.mu.Lock()
-    defer s.mu.Unlock()
+	if !survey.AllowBack {
+		return nil, fmt.Errorf("going back is not allowed for this survey")
+	}
 
-    progressKey := generateProgressKey(userID, surveyID)
-    currentIndex, exists := s.userProgress[progressKey]
-    if !exists || currentIndex == 0 {
-        return nil, fmt.Errorf("no previous questions available")
-    }
+	progressKey := generateProgressKey(userID, surveyID)
+	shownQuestionsKey := progressKey
 
-    questions, err := s.questionOps.GetQuestionsBySurveyID(ctx, surveyID)
-    if err != nil {
-        return nil, err
-    }
+	shownQuestions := s.shownQuestions[shownQuestionsKey]
+	if len(shownQuestions) <= 1 {
+		return nil, fmt.Errorf("no previous questions available")
+	}
 
-    if int(currentIndex) > len(questions) {
-        return nil, fmt.Errorf("you have reached the end of the questions")
-    }
+	var previousQuestionID uuid.UUID
 
-    s.userProgress[progressKey]--
+	if survey.IsRandom {
+		previousQuestionID = shownQuestions[len(shownQuestions)-2]
+		s.shownQuestions[shownQuestionsKey] = shownQuestions[:len(shownQuestions)-1]
+	} else {
+		currentIndex := s.userProgress[progressKey]
+		if currentIndex <= 1 {
+			return nil, fmt.Errorf("no previous questions available")
+		}
+		s.userProgress[progressKey]--
+		previousQuestionID = shownQuestions[currentIndex-2]
+	}
 
-    currentIndex = s.userProgress[progressKey]
-    return questions[currentIndex], nil
+	previousQuestion, err := s.questionOps.GetByID(ctx, previousQuestionID)
+	if err != nil {
+		return nil, err
+	}
+
+	return previousQuestion, nil
+}
+
+func (s *SurveyService) ResetUserProgress(userID string, surveyID uuid.UUID) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	progressKey := generateProgressKey(userID, surveyID)
+	delete(s.userProgress, progressKey)
+	delete(s.shownQuestions, progressKey)
+}
+
+func getNotShownQuestions(allQuestions []*question.Question, shownQuestionIDs []uuid.UUID) []*question.Question {
+	notShown := make([]*question.Question, 0)
+	shownMap := make(map[uuid.UUID]bool)
+
+	for _, id := range shownQuestionIDs {
+		shownMap[id] = true
+	}
+
+	for _, q := range allQuestions {
+		if !shownMap[q.ID] {
+			notShown = append(notShown, q)
+		}
+	}
+
+	return notShown
 }
 
 func (s *SurveyService) GetQuestion(ctx context.Context, questionID uuid.UUID) (*question.Question, error) {
